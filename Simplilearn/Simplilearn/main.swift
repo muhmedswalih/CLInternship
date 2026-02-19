6ae9387670a2f634da488f8719983f109e534a19
Salih <salihoffl01@gmail.com>
2026-02-19 11:11:28 +0530

    Swift Basic Coding

--- Simplilearn/Simplilearn/main.swift
+++ Simplilearn/Simplilearn/main.swift
@@ -1,11 +1,67 @@
-//
-//  main.swift
-//  Simplilearn
-//
-//  Created by MUHAMMAD SALIH on 19/02/26.
-//
+//VARIABLES
+//....................
 
-import Foundation
+var a = 5
+print("\(a)")
 
-print("Hello, World!")
+//STRING INTERPOLATION
 
+var name = "salih"
+print("hello \(name)!")
+
+//STRING CONCATENATION
+
+var x = "Hii"
+var y = "Salih"
+var z = x+y
+print(z)
+
+//CONDITIONAL STATEMENTS
+
+var num = 7
+ 
+if num>0{
+    print("NUMBER IS POSITIVE")
+}else if num<0{
+    print("NUMBER IS NEGATIVE")
+}
+else{
+    print("NUMBER IS ZERO")
+}
+
+//FOR LOOP
+
+for i in 1...5{
+    print(i)
+}
+
+for number in 1...5{
+    if number % 2 == 0{
+        print("\(number) IS EVEN")
+    }else{
+        print("\(number) IS ODD")
+    }
+}
+
+//WHILE LOOP
+
+var number = 5
+while number>0{
+    print("WHILE LOOP STARTED")
+    if number & 2 == 0{
+        print("\(number) is even")
+    }else{
+        print("\(number) is odd")
+    }
+    number -= 1
+}
+
+//ARRAYS
+
+var nameList:[String]=["Abhi","Anu","Alan"]
+print("\(nameList)")
+print("First Person is \(nameList[0])")//prints specified element
+
+nameList.append("Ajay")
+
+print("\(nameList)")
